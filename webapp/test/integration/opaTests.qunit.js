0d671a4b59afd8e1a59c8435213c69a55e44ef78
luis Dania <css.luism@gmail.com>
2024-04-25 20:38:14 +0000

    Initial Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["uikccdistcost/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"uikccdistcost/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
